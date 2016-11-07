27bfdf6f2268d5a24f684c56a48eac2ac9bb74b6
iyyel <Iyyelsec@gmail.com>
2016-11-07 10:29:02 +0100

    Refactoring Die, Player & BankAccount

--- 40_del3/src/spil/models/BankAccount.java
+++ 40_del3/src/spil/models/BankAccount.java
@@ -10,84 +10,20 @@ package spil.models;
 
 public final class BankAccount {
 
-	/**
-	 * Indkapslede klasse variabler, "fields."
-	 * 
-	 * @param MAX_COIN_AMOUNT
-	 *            Maksimal værdi af variable coins.
-	 * @param DEFAUL_COIN_AMOUNT
-	 *            Startværdien af variable coins.
-	 * @param MIN_COIN_AMOUNT
-	 *            Minimumsværdien af variable coins.
-	 * @param coins
-	 *            Antallet af mønter som kontoen har.
-	 */
 	private final int MAX_COIN_AMOUNT;
-	private final int DEFAULT_COIN_AMOUNT;
 	private final int MIN_COIN_AMOUNT;
 	private int coins;
 
-	/**
-	 * Constructor til Account klassen, som har tre variable inde i
-	 * parameterlisten.
-	 */
-	public BankAccount(int maxCoinAmount, int defaultCoinAmount, int minCoinAmount) {
+	public BankAccount(int maxCoinAmount, int minCoinAmount, int coins) {
 		this.MAX_COIN_AMOUNT = maxCoinAmount;
-		this.DEFAULT_COIN_AMOUNT = defaultCoinAmount;
 		this.MIN_COIN_AMOUNT = minCoinAmount;
-		coins = DEFAULT_COIN_AMOUNT;
+		this.coins = coins;
 	}
 
-	/**
-	 * Metode som indsætter amount til variable coins.
-	 * 
-	 * @param amount
-	 *            Mængden der bliver tilsat variable coins.
-	 */
 	public void addCoins(int amount) {
 		coins += amount;
 	}
 
-	/**
-	 * Metode der nulstiller variablen coins til DEFAULT_COIN_AMOUNT.
-	 */
-	public void reset() {
-		coins = DEFAULT_COIN_AMOUNT;
-	}
-
-	/**
-	 * Metode isFull() der returnerer en boolean om coins er større eller lig
-	 * med MAX_COIN_AMOUNT.
-	 * 
-	 * @return true Hvis coins er større eller lig med MAX_COIN_AMOUNT.
-	 * @return false Hvis coins ikke er større eller lig med MAX_COIN_AMOUNT.
-	 */
-	public boolean isFull() {
-		if (coins >= MAX_COIN_AMOUNT) {
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Metode isEmpty() der returnerer en boolean om coins er mindre eller lig
-	 * med MIN_COIN_AMOUNT.
-	 * 
-	 * @return true Hvis coins er mindre eller lig med MIN_COIN_AMOUNT.
-	 * @return false Hvis coins ikke er mindre eller lig med MIN_COIN_AMOUNT.
-	 */
-	public boolean isEmpty() {
-		if (coins <= MIN_COIN_AMOUNT) {
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * En getter metode der returnerer værdien af coins.
-	 *
-	 * @return coins Mængden af mønter som kontoen har.
-	 */
 	public int getCoins() {
 		return coins;
 	}
--- 40_del3/src/spil/models/Player.java
+++ 40_del3/src/spil/models/Player.java
@@ -10,81 +10,22 @@ package spil.models;
 
 public final class Player {
 
-	/**
-	 * Indkapslede klasse variabler, "fields."
-	 * 
-	 * @param name
-	 *            Navnet på spilleren.
-	 * @param acc
-	 *            Spillerens bank konto.
-	 * @param textBoundary
-	 *            textBoundary objekt for klassen.
-	 */
 	private String name;
 	private final BankAccount acc;
 
-	/**
-	 * Constructor for klassen Player med parameter name.
-	 *
-	 * Constructoren instantiere acc objektet samt textBoundary objektet, og
-	 * sætter den globale variable name til at være lig med name parameteren.
-	 */
-	public Player(String name) {
+	public Player(String name, int maxCoinAmount, int minCoinAmount, int coins) {
 		this.name = name;
-		acc = new BankAccount(3000, 1000, 0);
+		acc = new BankAccount(maxCoinAmount, minCoinAmount, coins);
 	}
 
-	/**
-	 * Wrapper metode for acc objektets isFull() metode. Hvis spillerens konto
-	 * er fuld, har spilleren vundet.
-	 */
-	public boolean hasWon() {
-		if (acc.isFull()) {
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Wrapper metode for acc objektets isEmpty() metode. Hvis spillerens konto
-	 * er tom, har spilleren tabt.
-	 */
-	public boolean hasLost() {
-		if (acc.isEmpty()) {
-		}
-		return false;
-	}
-
-	/**
-	 * Wrapper metode for acc objektets addCoins() metode. Ligger amount til
-	 * kontoens coins værdi.
-	 */
 	public void addCoins(int amount) {
 		acc.addCoins(amount);
 	}
 
-	/**
-	 * Wrapper metode for acc objektets reset() metode. Kontoens pengebeholdning
-	 * bliver nulstillet.
-	 */
-	public void resetAccount() {
-		acc.reset();
-	}
-
-	/**
-	 * Getter metode for klasse variable name.
-	 * 
-	 * @return name Spillerens navn.
-	 */
 	public String getName() {
 		return name;
 	}
 
-	/**
-	 * Wrapper og getter metode for spillerens pengebeholdning.
-	 * 
-	 * @return acc.getCoins() Spillerens pengebeholdning returneres.
-	 */
 	public int getCoins() {
 		return acc.getCoins();
 	}
