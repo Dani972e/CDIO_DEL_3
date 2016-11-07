7ff79cf4f9236e6ab9c9643ea3a3a9ae6f80f8de
iyyel <Iyyelsec@gmail.com>
2016-11-07 11:08:29 +0100

    Developing Field inheritance structure.

--- 40_del3/src/spil/fields/Ownable.java
+++ 40_del3/src/spil/fields/Ownable.java
@@ -1,5 +1,26 @@
 package spil.fields;
 
+import java.awt.Color;
+
+import spil.models.Player;
+
 public abstract class Ownable extends Field {
+
+	protected Player owner;
+	protected int price;
+
+	public Ownable(Color bgColor, Color fgColor, String title, String subText, String desc, Player owner, int price) {
+		super(bgColor, fgColor, title, subText, desc);
+		this.owner = owner;
+		this.price = price;
+	}
+
+	public Player getOwner() {
+		return owner;
+	}
+
+	public int getPrice() {
+		return price;
+	}
 
 }
