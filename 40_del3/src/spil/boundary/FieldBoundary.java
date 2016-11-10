d122cf454e056ea1de228d5b87e73a6292e3540f
Iyyel <iyyelsec@gmail.com>
2016-11-10 12:13:28 +0100

    Created Field colors.

--- 40_del3/src/spil/boundary/FieldBoundary.java
+++ 40_del3/src/spil/boundary/FieldBoundary.java
@@ -1,12 +1,20 @@
 package spil.boundary;
 
+import java.awt.Color;
+
 public final class FieldBoundary {
 
 	private FieldBoundary() {
 
 	}
 
-
-	
+	public static Color[][] fieldColors = { { Color.DARK_GRAY, Color.BLACK }, { Color.DARK_GRAY, Color.BLACK },
+			{ Color.DARK_GRAY, Color.BLACK }, { Color.DARK_GRAY, Color.BLACK }, { Color.DARK_GRAY, Color.BLACK },
+			{ Color.DARK_GRAY, Color.BLACK }, { Color.DARK_GRAY, Color.BLACK }, { Color.DARK_GRAY, Color.BLACK },
+			{ Color.DARK_GRAY, Color.BLACK }, { Color.DARK_GRAY, Color.BLACK }, { Color.DARK_GRAY, Color.BLACK },
+			{ Color.YELLOW, Color.BLACK }, { Color.YELLOW, Color.BLACK }, { Color.RED, Color.BLACK },
+			{ Color.RED, Color.BLACK }, { Color.BLUE, Color.BLACK }, { Color.BLUE, Color.BLACK },
+			{ Color.CYAN, Color.BLACK }, { Color.CYAN, Color.BLACK }, { Color.CYAN, Color.BLACK },
+			{ Color.CYAN, Color.BLACK }, };
 
 }
