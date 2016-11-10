d4f80535d42d9cfb2517163ddf39a4e1933aa2fe
Iyyel <iyyelsec@gmail.com>
2016-11-10 10:46:07 +0100

    test

--- 40_del3/src/spil/entity/PlayerList.java
+++ 40_del3/src/spil/entity/PlayerList.java
@@ -1,6 +1,5 @@
 package spil.entity;
 
-
 public class PlayerList {
 
 	private final int MAX_PLAYER_COUNT = 10;
@@ -10,15 +9,15 @@ public class PlayerList {
 
 	public PlayerList(int playerCount) {
 		playerList = new Player[playerCount];
-		if (playerCount > MAX_PLAYER_COUNT)
+
+		if (playerCount > MAX_PLAYER_COUNT) {
 			playerCount = MAX_PLAYER_COUNT;
-
-		if (playerCount < MIN_PLAYER_COUNT)
+		} else if (playerCount < MIN_PLAYER_COUNT) {
 			playerCount = MIN_PLAYER_COUNT;
+		}
 
-		for (int i = 0; i < playerCount; i++) 
-			playerList[i]=new Player(Integer.toString(i), 0, 0, 0);
-		
+		for (int i = 0; i < playerCount; i++)
+			playerList[i] = new Player(Integer.toString(i), 0, 0, 0);
 	}
 
 }
