cb129a714606208bc3a964fa8ea508a1d84ad5f2
dani972e <anusic@live.dk>
2016-11-14 16:22:10 +0100

    arrayLists

--- 40_del3/src/spil/entity/field/Ownable.java
+++ 40_del3/src/spil/entity/field/Ownable.java
@@ -1,9 +1,14 @@
 package spil.entity.field;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import spil.entity.Player;
 
 public abstract class Ownable extends Field {
 
+	private static List<Player> ownerList = new ArrayList<Player>(); 
+	
 	protected int price;
 	protected Player owner;
 	
