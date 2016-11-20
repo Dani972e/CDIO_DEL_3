e569a6eb2986b8915ccbd68d9df41dbc766d8435
Iyyel <iyyelsec@gmail.com>
2016-11-20 16:41:14 +0100

    Formatting 2nd testPlayer.

--- 40_del3/src/test/testPlayer2_DONTREMOVETHAT.java
+++ 40_del3/src/test/testPlayer2_DONTREMOVETHAT.java
@@ -1,6 +1,6 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.After;
 import org.junit.Before;
@@ -15,7 +15,7 @@ public class testPlayer2_DONTREMOVETHAT {
 	private Player playerTest;
 	private GameBoard gameBoard;
 	private PlayerList playerList;
-	
+
 	private final int MAX_BALANCE = 10000;
 	private final int MIN_BALANCE = 0;
 
@@ -23,16 +23,15 @@ public class testPlayer2_DONTREMOVETHAT {
 	public void setUp() throws Exception {
 		gameBoard = new GameBoard();
 		playerList = new PlayerList(6, 10000000, 0, 30000, 1, gameBoard.getRandomUniqueCars());
-		playerTest = new Player("Player Test",MAX_BALANCE, MIN_BALANCE, 1000, 0);
+		playerTest = new Player("Player Test", MAX_BALANCE, MIN_BALANCE, 1000, 0);
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		gameBoard=null;
-		playerList=null;
-		playerTest=null;
+		gameBoard = null;
+		playerList = null;
+		playerTest = null;
 	}
-
 
 	@Test // Negative Test
 	public void testBalanceOverflow() {
@@ -42,7 +41,7 @@ public class testPlayer2_DONTREMOVETHAT {
 		playerTest.removeBalance(playerTest.getBalance());
 		playerTest.addBalance(Integer.MAX_VALUE);
 		playerTest.addBalance(1);
-		
+
 		int actual = playerTest.getBalance();
 		assertEquals(expected, actual);
 	}
@@ -54,7 +53,7 @@ public class testPlayer2_DONTREMOVETHAT {
 		playerTest.removeBalance(playerTest.getBalance());
 		playerTest.addBalance(Integer.MIN_VALUE);
 		playerTest.removeBalance(1);
-		
+
 		int actual = playerTest.getBalance();
 		assertEquals(expected, actual);
 	}
@@ -65,15 +64,15 @@ public class testPlayer2_DONTREMOVETHAT {
 		int actual = playerList.getTotalPlayers();
 		assertEquals(expected, actual);
 
-		for(int i=5;i>0;--i){
+		for (int i = 5; i > 0; --i) {
 			playerList.removePlayer(playerList.getPlayer(i));
 			expected = i;
 			actual = playerList.getPlayersLeft();
 			assertEquals(expected, actual);
 		}
 
-		Player expected2=playerList.getPlayer(0);
-		Player actual2=playerList.getLastPlayer();
+		Player expected2 = playerList.getPlayer(0);
+		Player actual2 = playerList.getLastPlayer();
 		assertEquals(expected2, actual2);
 	}
 
